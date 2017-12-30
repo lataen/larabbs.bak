0fffbe6493d7eaa9ab6557044319808b306a0382
vagrant <vagrant@homestead>
2017-12-30 11:59:05 +0000

    Finish user auth

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,21 @@
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+
+
+// Auth::routes();  相当于下面 权限、注册、忘记密码、 这些路由的总合
+
+// 权限路由 ...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// 注册路由...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// 忘记密码路由...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
